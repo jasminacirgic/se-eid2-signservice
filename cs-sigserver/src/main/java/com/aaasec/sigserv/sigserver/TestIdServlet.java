c820f894c685f0875417267774c5aaf22f336089
sts@aaa-sec.com <sts@aaa-sec.com@1838efaa-b268-944f-0caa-88194a684989>
2013-05-31 13:12:33 +0000

    Import of reference code for eid2 sign service

---
+++ cs-sigserver/src/main/java/com/aaasec/sigserv/sigserver/TestIdServlet.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013 Swedish E-identification Board (E-legitimationsnämnden)
+ *  		 
+ *   Licensed under the EUPL, Version 1.1 or ñ as soon they will be approved by the 
+ *   European Commission - subsequent versions of the EUPL (the "Licence");
+ *   You may not use this work except in compliance with the Licence. 
+ *   You may obtain a copy of the Licence at:
+ * 
+ *   http://joinup.ec.europa.eu/software/page/eupl 
+ * 
+ *   Unless required by applicable law or agreed to in writing, software distributed 
+ *   under the Licence is distributed on an "AS IS" basis,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *   implied.
+ *   See the Licence for the specific language governing permissions and limitations 
+ *   under the Licence.
+ */
+package com.aaasec.sigserv.sigserver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+/**
+ * This servlet class provides test identities that are used only when the web service is run in devmode.
+ * The test identity is stored in a session cookie as a replacement for the SAML based authentication
+ * that is enforced in production mode. Dev mode is set through a parameter in the Web.xml file.
+ * @author stefan
+ */
+public class TestIdServlet extends HttpServlet {
+
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+
+        String action = request.getParameter("action");
+        if (action == null) {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            response.getWriter().write("");
+            return;
+        }
+
+        if (action.equals("userlist")) {
+            String json = TestSigIdentities.getUserInfo();
+            response.getWriter().write(json);
+            return;
+        }
+
+        if (action.equals("cookie")) {
+            String json = TestSigIdentities.getCookie(request, response);
+            response.getWriter().write(json);
+            return;
+        }
+
+        if (action.equals("resetCookie")) {
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("testSigID")) {
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+            response.getWriter().write("[]");
+            return;
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+
+}
